22236214134458a80b723ace79a6032dac62efa8
Rafael <rafaelgouvea219@gmail.com>
2023-02-15 17:55:55 -0300

    Mais atividades

--- Aula 04/src/Aula04/Atividade04.java
+++ Aula 04/src/Aula04/Atividade04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Atividade04 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner leia = new Scanner(System.in);
 		float matriz[][] = new float[3][4];
 		float media[] = new float[3];
 		float soma = 0.0f;
@@ -13,11 +13,11 @@ public class Atividade04 {
 		for (int linha = 0; linha < matriz.length; linha++) {
 			for (int coluna = 0; coluna < matriz[linha].length; coluna++) {
 				System.out.println("Digitar a nota ");
-				matriz[linha][coluna] = sc.nextFloat();
+				matriz[linha][coluna] = leia.nextFloat();
 			}
 
 		}
-		for(int linha = 0);
+		
 	}
 
 }
